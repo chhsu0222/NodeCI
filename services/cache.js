7cac8ab6338173d932ff75ec2fb2d2937818e6ed
ch <ch@mail.startek-eng.com>
2018-06-25 16:28:31 +0800

    Resolving values. (cache.js)

--- services/cache.js
+++ services/cache.js
@@ -7,15 +7,25 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-  console.log('IM ABOUT TO RUN A QUERY');
+mongoose.Query.prototype.exec = async function() {
   // do not accidentally modify the object that is returned
   // from getQuery or we will modify the actual query itself
-  const key = Object.assign({}, this.getQuery(), {
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
     collection: this.mongooseCollection.name
-  });
+  }));
 
-  console.log(key);
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.get(key);
 
-  return exec.apply(this, arguments);
+  // If we do, return that
+  if (cacheValue) {
+    console.log(cacheValue);
+  }
+
+  // Otherwise, issue the query and store the result in redis
+  const result = await exec.apply(this, arguments);
+  // notice that result is a mongoose document or model instance
+  client.set(key, JSON.stringify(result));
+
+  return result;
 };
