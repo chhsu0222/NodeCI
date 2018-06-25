711b6b6bfde3abf456ce57c18ec29e2458a23095
ch <ch@mail.startek-eng.com>
2018-06-25 15:24:23 +0800

    Key creation. (cache.js)

--- services/cache.js
+++ services/cache.js
@@ -4,9 +4,13 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function() {
   console.log('IM ABOUT TO RUN A QUERY');
+  // do not accidentally modify the object that is returned
+  // from getQuery or we will modify the actual query itself
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name
+  });
 
-  console.log(this.getQuery());
-  console.log(this.mongooseCollection.name);
+  console.log(key);
 
   return exec.apply(this, arguments);
 };
