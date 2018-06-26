2dcd06910a402c5f6a9bd66b16989b654361b4a0
ch <ch@mail.startek-eng.com>
2018-06-26 17:34:19 +0800

    Toggleable Cache (cache.js)

--- services/cache.js
+++ services/cache.js
@@ -7,7 +7,16 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function() {
+  this.useCache = true;
+  // To make cache a chainable function call
+  return this;
+}
+
 mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
   // do not accidentally modify the object that is returned
   // from getQuery or we will modify the actual query itself
   const key = JSON.stringify(Object.assign({}, this.getQuery(), {
