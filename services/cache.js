586696c671147010f7dc892eb8285e807e2c10f8
ch <ch@mail.startek-eng.com>
2018-07-03 16:42:50 +0800

    Nested Hashes (cache.js & blogRoutes.js)

--- services/cache.js
+++ services/cache.js
@@ -4,11 +4,12 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function() {
+mongoose.Query.prototype.cache = function(options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
   // To make cache a chainable function call
   return this;
 }
@@ -24,7 +25,7 @@ mongoose.Query.prototype.exec = async function() {
   }));
 
   // See if we have a value for 'key' in redis
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   // If we do, return that
   if (cacheValue) {
@@ -39,7 +40,8 @@ mongoose.Query.prototype.exec = async function() {
   // Otherwise, issue the query and store the result in redis
   const result = await exec.apply(this, arguments);
   // notice that result is a mongoose document or model instance
-  client.set(key, JSON.stringify(result));
+  client.hset(this.hashKey, key, JSON.stringify(result));
+  client.expire(this.hashKey, 10);
 
   return result;
 };
