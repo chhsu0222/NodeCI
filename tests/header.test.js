8fe73294f07b68dbca7e134edc1bc3a8b7ad020d
ch <ch@mail.startek-eng.com>
2018-07-06 14:19:07 +0800

    Chromium navigation. (header.test.js)

--- tests/header.test.js
+++ tests/header.test.js
@@ -10,4 +10,5 @@ test('We can launch a browser', async () => {
     headless: false     // Enable graphical UI
   });
   const page = await browser.newPage();
+  await page.goto('localhost:3000');
 });
