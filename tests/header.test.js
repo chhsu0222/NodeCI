18d946fa9b19532c0ed26f0737f7af1ed3d5ce3b
ch <ch@mail.startek-eng.com>
2018-07-06 14:07:40 +0800

    Launching Chromium instances with puppeteer. (header.test.js)

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,13 @@
+const puppeteer = require('puppeteer');
+
 test('Adds two numbers', () => {
   const sum = 1 + 2;
   expect(sum).toEqual(3);
+});
+
+test('We can launch a browser', async () => {
+  const browser = await puppeteer.launch({
+    headless: false     // Enable graphical UI
+  });
+  const page = await browser.newPage();
 });
