23cdb00ab43df25dc6ecdd39f6574a580b8b2ec3
ch <ch@mail.startek-eng.com>
2018-07-23 15:20:07 +0800

    120. Part 2 - Super advanced test helper. (page.js & blogs.test.js)

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -64,15 +64,27 @@ describe('When logged in', async () => {
 });
 
 describe('User is not logged in', async () => {
-  test('User cannot create blog posts', async () => {
-    const result = await page.post('/api/blogs', { title: 'T', content: 'C' });
+  const actions = [
+    {
+      method: 'get',
+      path: '/api/blogs'
+    },
+    {
+      method: 'post',
+      path: '/api/blogs',
+      data: {
+        title: 'T',
+        content: 'C'
+      }
+    }
+  ];
 
-    expect(result).toEqual({ error: 'You must log in!' });
+  test('Blog related actions are prohibited', async () => {
+    const results = await page.execRequests(actions);
+
+    for (let result of results) {
+      expect(result).toEqual({ error: 'You must log in!' });
+    }
   });
 
-  test('User cannot get a list of posts', async () => {
-    const result = await page.get('/api/blogs');
-
-    expect(result).toEqual({ error: 'You must log in!' });
-  });
 });
