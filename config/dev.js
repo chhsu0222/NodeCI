6adc48368bf20f4d53f0ecc263b995cd24b02371
ch <ch@mail.startek-eng.com>
2018-07-25 17:38:20 +0800

    129. Setting the RedisURL for dev mode. (dev.js)

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,6 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://ch:1234abcd@ds255930.mlab.com:55930/blog_dev',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
